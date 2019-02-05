33fff07967fc546e322c9e7c0d77ff3602cd09d0
Mina Mekhael <mina.mekhael@ymail.com>
2019-02-05 01:05:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,7 +6,6 @@ class Song
 
   @@all = []
 
-  def initialize(name, artist = nil, genre = nil)
   def initialize(name, artist=nil, genre=nil)
     @name = name
     self.artist = artist if artist
@@ -27,46 +26,21 @@ class Song
     self.genre = genre if genre != nil
   end
 
-  def self.find_or_create_by_name(name)
-    self.find_by_name(name) || self.create(name)
   def self.all 
     @@all.uniq
   end
 
-  def self.new_from_filename(filename)
-    parts = filename.split(" - ")
-    artist_name, song_name, genre_name = parts.first, parts[1], parts[2].gsub(".mp3", "")
 
-    artist = Artist.find_or_create_by_name(artist_name)
-    genre = Genre.find_or_create_by_name(genre_name)
-    self.new(song_name, artist, genre)
   def self.destroy_all 
     @@all.clear
   end
 
-  def self.create_from_filename(filename)
-    parts = filename.split(" - ")
-    artist_name, song_name, genre_name = parts.first, parts[1], parts[2].gsub(".mp3", "")
-
-    artist = Artist.find_or_create_by_name(artist_name)
-    genre = Genre.find_or_create_by_name(genre_name)
-    self.create(song_name, artist, genre)
-  def save
-    @@all << self unless @@all.include?(self)
-  end
-
   def self.destroy_all
     @@all.clear
-  def self.create(song, artist=nil, genre=nil)
-    new_song = Song.new(song, artist, genre)
-    new_song.save
-    new_song
   end
-
+  
   def artist=(artist)
-@@ -52,15 +35,19 @@ def artist=(artist)
-  end
-
+    
   def genre=(genre)
     @genre = genre
     @genre = genre 
